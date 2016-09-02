412bcdaeec14413790aedff8bf1fb611551fd8ca
Jonas Daniels <jonas.daniels@outlook.com>
2016-09-02 12:16:54 -0700

    updated config

--- config/paths.js
+++ config/paths.js
@@ -36,7 +36,7 @@ if (isInCreateReactAppSource) {
   // before eject: we're in ./node_modules/react-scripts/config/
   module.exports = {
     appBuild: resolveApp('build'),
-    appHtml: resolveApp('index.html'),
+    appHtml: resolveApp('src/index.ejs'),
     appPackageJson: resolveApp('package.json'),
     appSrc: resolveApp('src'),
     appNodeModules: resolveApp('node_modules'),
@@ -47,7 +47,7 @@ if (isInCreateReactAppSource) {
   // after eject: we're in ./config/
   module.exports = {
     appBuild: resolveApp('dist'),
-    appHtml: resolveApp('src/index-template.ejs'),
+    appHtml: resolveApp('src/index.ejs'),
     appPackageJson: resolveApp('package.json'),
     appSrc: resolveApp('src'),
     appNodeModules: resolveApp('node_modules'),
